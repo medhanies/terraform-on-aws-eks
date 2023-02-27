652546e2045839b2faac7101c2a9f635756c0d93
medhanies <medhaniesolomon@yahoo.com>
2023-02-26 21:22:15 -0600

    1

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
@@ -6,8 +6,8 @@ data "aws_ec2_instance_type_offerings" "my_ins_type1" {
   }
   filter {
     name   = "location"
-    #values = ["us-east-1a"]
-    values = ["us-east-1e"]
+    values = ["us-east-1a"]
+    # values = ["us-east-1e"]
   }
   location_type = "availability-zone"
 }
