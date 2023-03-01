3c80b885e5bac34d9029af68cfbbc8154aeca178
medhanies <medhaniesolomon@yahoo.com>
2023-02-28 21:52:14 -0600

    02/28

--- 07-AWS-EC2-BastionHost/terraform-manifests/c4-03-ec2bastion-securitygroups.tf
+++ 07-AWS-EC2-BastionHost/terraform-manifests/c4-03-ec2bastion-securitygroups.tf
@@ -2,7 +2,7 @@
 # Security Group for Public Bastion Host
 module "public_bastion_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.5.0"
+  version = "4.17.1"
 
   name = "${local.name}-public-bastion-sg"
   description = "Security Group with SSH port open for everybody (IPv4 CIDR), egress ports are all world open"
--- 07-AWS-EC2-BastionHost/terraform-manifests/c4-05-ec2bastion-instance.tf
+++ 07-AWS-EC2-BastionHost/terraform-manifests/c4-05-ec2bastion-instance.tf
@@ -3,7 +3,7 @@
 module "ec2_public" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   #version = "~> 3.0"
-  version = "3.3.0"
+  version = "4.3.0"
 
   name = "${local.name}-BastionHost"
   ami                    = data.aws_ami.amzlinux2.id
